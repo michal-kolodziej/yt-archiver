5763d6660e2b1104fe219800ec6d86ffcedbaded
Artur Kuś <arturkus98@gmail.com>
2023-09-18 14:58:21 +0200

    Adding homepage messaging system and fixing checking invalid url method

--- src/main/java/com/wheeler/ytarchiver/homepage/HomeController.java
+++ src/main/java/com/wheeler/ytarchiver/homepage/HomeController.java
@@ -1,6 +1,5 @@
 package com.wheeler.ytarchiver.homepage;
 
-import com.google.common.base.Strings;
 import com.wheeler.ytarchiver.downloader.binary.youtubedl.VideoQualityService;
 import com.wheeler.ytarchiver.homepage.message.HomepageMessageService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URL;
 
 @Controller
 @RequestMapping("/")
@@ -33,21 +34,25 @@ public class HomeController {
 
     @PostMapping("/download/mp3")
     public String downloadMp3(DownloadFormData downloadFormData) {
-        if (isUrlInvalid(downloadFormData.getUrl())) {
-            return "redirect:/home?error=INVALID_URL";
-        }
-        return "redirect:/api/download/mp3?url=" + downloadFormData.getUrl();
+        return isUrlInvalid(downloadFormData.getUrl())
+                ? "redirect:/home?error=INVALID_URL"
+                : "redirect:/api/download/mp3?url=" + downloadFormData.getUrl();
     }
 
     @PostMapping("/download/mp4")
     public String downloadMp4(DownloadFormData downloadFormData) {
-        if (isUrlInvalid(downloadFormData.getUrl())) {
-            return "redirect:/home?error=INVALID_URL";
-        }
-        return "redirect:/api/download/mp4?url=" + downloadFormData.getUrl() + "&quality=" + downloadFormData.getQuality();
+        return isUrlInvalid(downloadFormData.getUrl())
+                ? "redirect:/home?error=INVALID_URL"
+                : "redirect:/api/download/mp4?url=" + downloadFormData.getUrl() + "&quality=" + downloadFormData.getQuality();
     }
 
-    private boolean isUrlInvalid(String url) {
-        return Strings.isNullOrEmpty(url);
+    private boolean isUrlInvalid(String urlAsString) {
+        try {
+            URL url = new URL(urlAsString);
+            url.toURI();
+            return false;
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
--- src/main/java/com/wheeler/ytarchiver/homepage/message/HomepageMessage.java
+++ src/main/java/com/wheeler/ytarchiver/homepage/message/HomepageMessage.java
@@ -1,12 +1,15 @@
 package com.wheeler.ytarchiver.homepage.message;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-class HomepageMessage {
-    private String date;
+@AllArgsConstructor
+public class HomepageMessage {
+    private String header;
     private String message;
-
+    private String date;
+    private String author;
 }
